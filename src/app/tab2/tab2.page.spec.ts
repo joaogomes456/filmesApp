af7abe2f9b03ae5a6943aa294f582b768a005540
Etec 244 <joao.pontes29@etec.sp.gov.br>
2021-09-17 15:09:15 -0300

    17/09/2021
    Alert e Toast adicionados

--- src/app/tab2/tab2.page.spec.ts
+++ src/app/tab2/tab2.page.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
+
 import { Tab2Page } from './tab2.page';
 
 describe('Tab2Page', () => {
@@ -23,3 +24,4 @@ describe('Tab2Page', () => {
     expect(component).toBeTruthy();
   });
 });
+
